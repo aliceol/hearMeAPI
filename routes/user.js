8affff6f1d55fc3a7e53d628d1bed669ba246c27
Yacha Toueg <yachatoueg@Yachas-MacBook-Pro.local>
2018-08-10 12:39:21 +0200

    Fixed photo upload

--- routes/user.js
+++ routes/user.js
@@ -204,10 +204,17 @@ router.get("/getMyInfo", isAuthenticated, function(req, res) {
 
 router.post("/uploadPicture", isAuthenticated, uploadPictures, function(
   req,
-  res
+  res,
+  next
 ) {
   if (req.pictures.length) {
-    res.json(req.pictures[0]);
+    req.user.account.profilePic = req.pictures[0];
+    req.user.save(err => {
+      if (!err) {
+        return res.json(req.pictures[0]);
+      }
+      return next(err.message);
+    });
   } else {
     res.json({ error: "there is an error" });
   }
