d37774236a0694fec0aef14b2f8618e2735de0bf
Alice Olle-Laprune <alice.ollelaprune@gmail.com>
2018-08-13 18:12:56 +0200

    popular route updated to improve performances

--- components/getArtistImage.js
+++ components/getArtistImage.js
@@ -3,7 +3,6 @@ const scrapeIt = require("scrape-it");
 getArtistImage = link =>
   new Promise((resolve, reject) => {
     scrapeIt(link, {
-      title: "h1 > span > a",
       image: {
         listItem: ".profile-picture-wrap > a",
         data: {
@@ -14,11 +13,10 @@ getArtistImage = link =>
         }
       }
     }).then(({ data, response }) => {
-      console.log(`Status Code: ${response.statusCode}`);
       if (response.statusCode !== 200) {
         reject({ error: "an error occured ⚠️" });
       } else {
-        resolve(data);
+        resolve(data.image);
       }
     });
   });
